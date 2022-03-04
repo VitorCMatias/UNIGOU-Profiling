ca8ed96b01738c33fcf6d33688bab02adc3898c7
Vitor <vitorcosta.matias@usp.br>
2022-03-04 00:59:16 -0300

    merge DWT
    DWT now get the data and compute the statistics

--- Core/Inc/DWT.h
+++ Core/Inc/DWT.h
@@ -9,17 +9,40 @@
 #define DWT_H_
 #include "stm32f4xx_hal.h"
 
-/*#define start_dwt() DWT->CTRL |= dwt_ctrl
-#define stop_dwt() DWT->CTRL &= ~dwt_ctrl*/
+/*
+ *
+ * 	#define start_dwt() DWT->CTRL |= dwt_ctrl
+ *	#define stop_dwt() DWT->CTRL &= ~dwt_ctrl
+ *
+ */
+#define MEASUREMENT_MAX_SAMPLES 10
+struct data
+{
+    uint32_t cycles;
+    uint32_t sleep_cycles;
+    uint32_t exception_cycles;
 
+    uint32_t lsu_cycles;
+    uint32_t fold_counter;
+    uint32_t cpi_cycles;
+};
+typedef struct data DATA;
 
 class dwt {
 private:
     uint32_t dwt_ctrl = 0; /* bit mask for DWT->CTRL */
+
+    DATA measurements[MEASUREMENT_MAX_SAMPLES];
+    DATA avg;
+    uint8_t position;
 public:
 	dwt();
 	void start();
 	void stop();
+
+    void store_result();
+    void compute_statistics();
+    void set_position(uint8_t position);
 };
 
 #endif /* DWT_H_ */
--- Core/Src/DWT.cpp
+++ Core/Src/DWT.cpp
@@ -28,6 +28,8 @@ dwt::dwt()
     dwt_ctrl |= DWT_CTRL_LSUEVTENA_Msk;
     dwt_ctrl |= DWT_CTRL_FOLDEVTENA_Msk;
     dwt_ctrl |= DWT_CTRL_CPIEVTENA_Msk;
+
+    this->position = 0;
 }
 
 void dwt::start(){
@@ -36,4 +38,44 @@ void dwt::start(){
 void dwt::stop(){
 	DWT->CTRL &= ~dwt_ctrl;
 
+}
+
+
+void dwt::set_position(uint8_t position)
+{
+    this->position = position;
+}
+void dwt::store_result()
+{
+    this->measurements[this->position].cycles = DWT->CYCCNT;
+    this->measurements[this->position].sleep_cycles = DWT->SLEEPCNT;
+    this->measurements[this->position].exception_cycles = DWT->EXCCNT;
+
+    this->measurements[this->position].lsu_cycles = DWT->LSUCNT;
+    this->measurements[this->position].fold_counter = DWT->FOLDCNT;
+    this->measurements[this->position].cpi_cycles = DWT->CPICNT;
+
+    this->position++;
+}
+
+void dwt::compute_statistics()
+{
+    for (int position = 0; position < MEASUREMENT_MAX_SAMPLES; position++)
+    {
+        this->avg.cycles += this->measurements[position].cycles;
+        this->avg.sleep_cycles += this->measurements[position].sleep_cycles;
+        this->avg.exception_cycles += this->measurements[position].exception_cycles;
+
+        this->avg.lsu_cycles += this->measurements[position].lsu_cycles;
+        this->avg.fold_counter += this->measurements[position].fold_counter;
+        this->avg.cpi_cycles += this->measurements[position].cpi_cycles;
+    }
+
+    this->avg.cycles /= MEASUREMENT_MAX_SAMPLES;
+    this->avg.sleep_cycles /= MEASUREMENT_MAX_SAMPLES;
+    this->avg.exception_cycles /= MEASUREMENT_MAX_SAMPLES;
+
+    this->avg.lsu_cycles /= MEASUREMENT_MAX_SAMPLES;
+    this->avg.fold_counter /= MEASUREMENT_MAX_SAMPLES;
+    this->avg.cpi_cycles /= MEASUREMENT_MAX_SAMPLES;
 }
